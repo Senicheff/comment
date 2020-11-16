3f037cfdea33aa9f425bea4585e6e8f5cdec7642
Senicheff <dmitry@senicheff.ru>
2020-11-16 12:46:03 +0300

    Готовое ДЗ

--- comment.php
+++ comment.php
@@ -1,12 +1,33 @@
 <?php
 
-if (isset($_POST['author']) && isset($POST['comment']))
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
+
+
+if (isset($_POST['author']) && isset($_POST['comment']))
 {
     if(!empty($_POST['author']) && !empty($_POST['comment']))
     {
         $author  = trim($_POST['author']);
         $comment = trim ($_POST['comment']);
-        $date = time();
+        
+
+        $comment = "Автор: " . $author . " || " . date("d-m-Y H:i:s", time()) . " || " . $comment . "\n";
+        $data = "comments.txt";
+
+        if(is_file($data) && is_writable($data))
+        {
+            $storage = fopen($data, 'a') or die("Невозможно открыть файл!");
+            fwrite($storage, $comment);
+            fclose($storage);
+            header("Location: index.php?status=send");
+            
+        }
+        else
+        {
+            header("Location: index.php?status=error");    
+        }
     }
 }
 
--- index.php
+++ index.php
@@ -1,21 +1,54 @@
+<?php
+
+if(isset($_GET['status']) && !empty($_GET['status'])) 
+{
+    header("refresh:5;url=/");
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Форма отправки комментариев</title>
 </head>
+
 <body>
 
-<form action="comment.php" method="post">
-
-<input type="text" name="author" required>
-<input type="text" name="comment">
-<input type="text">
-
-<button type="submit">Добавить комментарий</button>
-
-</form>
+    <?php
     
+    $data = file("comments.txt") or die ("Невозможно открыть файл!");
+
+    foreach($data as $comment)
+    {
+        echo $comment . "<br>";
+    }
+    
+    ?>
+    <br>
+    <div class="comment_form">
+        <form action="comment.php" method="post">
+            <input type="text" name="author" placeholder="Автор" required>
+            <input type="text" name="comment" placeholder="Текст комментария" required>
+            <button type="submit">Добавить комментарий</button>
+        </form>
+        <?php
+
+        if ($_GET['status'] == "send") 
+        {
+            echo "<h2>Комментарий успешно добавлен!</h2>";
+        }
+        elseif ($_GET['status'] == "error")
+        {
+            echo "<h2>Произошла ошибка! Комментарий не добавлен!</h2>";
+        }
+
+        ?>
+    </div>
+
 </body>
+
 </html>
